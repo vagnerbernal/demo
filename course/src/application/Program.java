0769d81d75c1cd7b3d48e5346db78993bf22fe96
Vagner Bernal <vagnerbernal@gmail.com>
2021-07-15 23:50:17 -0300

    Mensagem de boa tarde

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -5,6 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Boa noite!");
 		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
 	}
-
 }
